8d601f759caffbd1fb25daa25051213dec9b2458
Nur Kholid <cocokcocok33@gmail.com>
2022-05-27 00:09:56 +0700

    feat: add dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Post, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Body,Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import {AuthDto} from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signUp(@Req() req: Request) {
-    console.log(req);
+  signUp(@Body() req:AuthDto) {
+    console.log({
+      email: req.email,
+      password: req.password
+    });
     return this.authService.signup();
   }
 
